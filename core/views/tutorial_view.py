619e4072f2cd5975377e2ae728d48ab6bf425887
thiagovl <contatothiagolemes@gmail.com>
2021-12-08 20:35:19 -0300

    Deploy Heroku

--- core/views/tutorial_view.py
+++ core/views/tutorial_view.py
@@ -9,10 +9,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login_page/')
 def tutorial(request, tec):
-    tutorials = requests.get(settings.URL_ENV_TUTORIALS).json()
+    tutorials = requests.get(settings.URL_ENV_TUTORIALS)
+    aux = tutorials.json()
     context = {
-        'dts': tutorials,
-        'tec': tec
+        'dts': aux,
+        # 'tec': tec
     }
     return render(request, 'tutorials_view.html', context)
 
